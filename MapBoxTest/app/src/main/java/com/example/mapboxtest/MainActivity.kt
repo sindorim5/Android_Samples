f90b400541825378c305d66b87a235f3f9cac049
sindorim5 <kihunbuzz@naver.com>
2023-03-07 17:06:17 +0900

    gradle.properties

--- MapBoxTest/app/src/main/java/com/example/mapboxtest/MainActivity.kt
+++ MapBoxTest/app/src/main/java/com/example/mapboxtest/MainActivity.kt
@@ -2,12 +2,38 @@ package com.example.mapboxtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mapboxtest.databinding.ActivityMainBinding
+import com.mapbox.maps.MapView
+import com.mapbox.maps.Style
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mapView: MapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        mapView = binding.mapView.apply {
+            getMapboxMap().loadStyleUri(Style.DARK)
+        }
     }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+
 }
